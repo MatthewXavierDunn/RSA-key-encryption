1b3165a326cb4d153b72e4b9d4a9a35d2a2f7e71
zuccthebigone <matthew.dunn.bkk@gmail.com>
2021-08-17 18:58:49 +0100

    changed name to bench.rs

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,13 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use num_bigint::BigUint;
 use rsa::{seeded_gen_rsa_keysets, rsa_decrypt, rsa_encrypt, seeded_gen_prime};
 
-fn encrypt_decrypt(
-    public_key: impl Into<BigUint>,
-    private_key: impl Into<BigUint>,
-    modulus: impl Into<BigUint>,
-) {
-    let modulus = modulus.into();
+fn encrypt_decrypt() {
+    let ((public_key, modulus), private_key) = seeded_gen_rsa_keysets(256);
     let msg = 12345u128;
     let ct = rsa_encrypt(msg, public_key, modulus.clone());
     black_box(rsa_decrypt(ct, private_key, modulus.clone()));
@@ -18,10 +13,8 @@ fn fast_prime_gen() {
 }
 
 fn benchmark(c: &mut Criterion) {
-    let ((public_key, modulus), private_key) = seeded_gen_rsa_keysets(256);
-    dbg!(&public_key, &modulus, &private_key);
     c.bench_function("encrypt_decrypt", |b| {
-        b.iter(|| encrypt_decrypt(public_key.clone(), private_key.clone(), modulus.clone()))
+        b.iter(|| encrypt_decrypt())
     });
     c.bench_function("fast_prime_gen", |b| {
         b.iter(|| fast_prime_gen())
